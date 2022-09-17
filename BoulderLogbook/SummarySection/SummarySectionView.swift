4edea3cb85d51af1a55b55f000eda85751677822
Martin List <martinlist@me.com>
2022-09-17 22:37:44 +0200

    Refactors `SummarySectionView`

--- BoulderLogbook/SummarySection/SummarySectionView.swift
+++ BoulderLogbook/SummarySection/SummarySectionView.swift
@@ -19,40 +19,37 @@ struct SummarySectionView: View {
                         state: \.entryStates,
                         action: SummarySectionAction.entryAction(id:action:))
                 ) { entryStore in
-                    summaryEntryView(for: entryStore)
-                        .swipeActions {
-                            Button(role: .destructive) {
-                                viewStore.send(.delete(ViewStore(entryStore).entry))
-                            } label: {
-                                Label("Delete", systemImage: "trash")
-                            }
-                            Button {
-                                viewStore.send(.edit(ViewStore(entryStore).entry))
-                            } label: {
-                                Label("Edit", systemImage: "pencil")
-                            }
-                            .tint(.orange)
+                    NavigationLink(value: entryStore) {
+                        SummaryEntryView(entry: ViewStore(entryStore).entry)
+                    }
+                    .swipeActions {
+                        Button(role: .destructive) {
+                            viewStore.send(.delete(ViewStore(entryStore).entry))
+                        } label: {
+                            Label("Delete", systemImage: "trash")
                         }
+                        Button {
+                            viewStore.send(.edit(ViewStore(entryStore).entry))
+                        } label: {
+                            Label("Edit", systemImage: "pencil")
+                        }
+                        .tint(.orange)
+                    }
                 }
             } header: {
-                Text(viewStore.date, format: .dateTime.year().month(.wide))
+                Text(
+                    viewStore.date,
+                    format: .dateTime.year().month(.wide)
+                )
             }
             .headerProminence(.increased)
         }
     }
 }
 
-extension SummarySectionView {
-    @ViewBuilder func summaryEntryView(for entryStore: Store<EntryState, EntryAction>) -> some View {
-        NavigationLink(value: entryStore) {
-            SummaryEntryView(entry: ViewStore(entryStore).entry)
-        }
-    }
-}
-
 struct SummarySectionView_Previews: PreviewProvider {
     static var previews: some View {
-        NavigationView {
+        NavigationStack {
             List {
                 SummarySectionView(
                     store: Store(
