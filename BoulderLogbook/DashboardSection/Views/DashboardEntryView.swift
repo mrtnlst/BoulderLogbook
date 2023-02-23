07566170968654afe6dc38b8b2b47ba32523b369
Martin List <martinlist@me.com>
2023-02-23 11:39:09 +0100

    Removes icon view from DashboardEntryView

--- BoulderLogbook/DashboardSection/Views/DashboardEntryView.swift
+++ BoulderLogbook/DashboardSection/Views/DashboardEntryView.swift
@@ -12,49 +12,37 @@ struct DashboardEntryView: View {
     let gradeSystem: GradeSystem
     
     var body: some View {
-        HStack {
-            iconView()
-            VStack(alignment: .leading) {
-                HStack {
-                    EntryColorView(
-                        tops: entry.tops,
-                        gradeSystem: gradeSystem
-                    )
-                    HStack(spacing: 2) {
-                        Text("×")
-                            .font(.footnote.weight(.medium))
-                        Text("\(entry.tops.filter { !$0.isAttempt }.count)")
-                            .font(.footnote.weight(.medium))
-                    }
+        VStack(alignment: .leading) {
+            HStack {
+                EntryColorView(
+                    tops: entry.tops,
+                    gradeSystem: gradeSystem
+                )
+                HStack(spacing: 2) {
+                    Text("×")
+                        .font(.footnote.weight(.medium))
+                    Text("\(entry.tops.filter { !$0.isAttempt }.count)")
+                        .font(.footnote.weight(.medium))
                 }
-                Text(entry.date, format: .dateTime.year().month().day().hour().minute())
-                    .font(.footnote)
-                    .foregroundColor(.secondary)
-                    .padding(.leading, 2)
             }
+            Text(entry.date, format: .dateTime.year().month().day().hour().minute())
+                .font(.footnote)
+                .foregroundColor(.secondary)
+                .padding(.leading, 2)
         }
         .padding(.vertical, 8)
     }
 }
-
-extension DashboardEntryView {
-    @ViewBuilder func iconView() -> some View {
-        ZStack {
-            Image(systemName: "figure.climbing")
-                .foregroundColor(.accentColor)
-            Color.accentColor.opacity(0.15)
-                .cornerRadius(8)
-        }
-        .frame(maxWidth: 48)
-    }
-}
-
 
 struct DashboardEntryView_Previews: PreviewProvider {
     static var previews: some View {
         List {
             DashboardEntryView(
                 entry: [Logbook.Section.Entry].samples[0],
+                gradeSystem: .mandala
+            )
+            DashboardEntryView(
+                entry: [Logbook.Section.Entry].samples[3],
                 gradeSystem: .mandala
             )
         }
